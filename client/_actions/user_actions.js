40db30b7fb8a632c0067e379ea20683dd8b605a8
AHNBelieve <an0731an@cau.ac.kr>
2024-10-18 17:04:28 +0900

    일단 여기까지 하자. Redux에서  Promise Action이 제대로 처리 안 되는 거 해결 필요.

--- client/_actions/user_actions.js
+++ client/_actions/user_actions.js
@@ -5,8 +5,9 @@ export function loginUser(dataToSubmit) {
   const request = axios
     .post("/api/users/login", dataToSubmit)
     .then((response) => response.data);
+  console.log(request);
   return {
-    type: LOGIN_USER,
+    type: "LOGIN_USER",
     payload: request,
   };
 }
